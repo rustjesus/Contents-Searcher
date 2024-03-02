e9b27bafecc9b740dd2e66a9b3b29a974eb40053
rustjesus <yeric12345@gmail.com>
2024-03-01 19:51:16 -0600

    Add project files.

---
+++ Contents Searcher/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Contents_Searcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchLocationBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.consoleOutRichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.searchStringTextBox1 = new System.Windows.Forms.TextBox();
+            this.searchButton1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchLocationBox1
+            // 
+            this.searchLocationBox1.Location = new System.Drawing.Point(80, 12);
+            this.searchLocationBox1.Name = "searchLocationBox1";
+            this.searchLocationBox1.Size = new System.Drawing.Size(530, 20);
+            this.searchLocationBox1.TabIndex = 0;
+            this.searchLocationBox1.TextChanged += new System.EventHandler(this.searchLocationBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search Dir:";
+            // 
+            // consoleOutRichTextBox1
+            // 
+            this.consoleOutRichTextBox1.Location = new System.Drawing.Point(116, 76);
+            this.consoleOutRichTextBox1.Name = "consoleOutRichTextBox1";
+            this.consoleOutRichTextBox1.Size = new System.Drawing.Size(494, 335);
+            this.consoleOutRichTextBox1.TabIndex = 2;
+            this.consoleOutRichTextBox1.Text = "";
+            // 
+            // searchStringTextBox1
+            // 
+            this.searchStringTextBox1.Location = new System.Drawing.Point(80, 38);
+            this.searchStringTextBox1.Name = "searchStringTextBox1";
+            this.searchStringTextBox1.Size = new System.Drawing.Size(530, 20);
+            this.searchStringTextBox1.TabIndex = 3;
+            this.searchStringTextBox1.TextChanged += new System.EventHandler(this.searchStringTextBox1_TextChanged);
+            // 
+            // searchButton1
+            // 
+            this.searchButton1.Location = new System.Drawing.Point(12, 64);
+            this.searchButton1.Name = "searchButton1";
+            this.searchButton1.Size = new System.Drawing.Size(75, 23);
+            this.searchButton1.TabIndex = 4;
+            this.searchButton1.Text = "Search";
+            this.searchButton1.UseVisualStyleBackColor = true;
+            this.searchButton1.Click += new System.EventHandler(this.searchButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Search String:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchButton1);
+            this.Controls.Add(this.searchStringTextBox1);
+            this.Controls.Add(this.consoleOutRichTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchLocationBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchLocationBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox consoleOutRichTextBox1;
+        private System.Windows.Forms.TextBox searchStringTextBox1;
+        private System.Windows.Forms.Button searchButton1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Contents Searcher/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contents_Searcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string folderPath = "Assets"; // The folder path to search.
+        private string searchString = ""; // The string to check for.
+        private bool caseSensitive = false; // Toggle for case sensitivity
+        private bool recursiveSearch = true; // Toggle for recursive searching
+        private string result;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> searchResults = new List<string>();
+        
+        private void SearchInFolder(string currentFolder)
+        {
+            searchResults.Clear(); // Clear previous search results
+
+            StringComparison comparisonType = caseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+            SearchOption searchOption = recursiveSearch ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            if (Directory.Exists(currentFolder))
+            {
+                string[] files = Directory.EnumerateFiles(currentFolder, "*.*", searchOption)
+                        .Where(file => file.EndsWith(".h") || file.EndsWith(".cpp"))
+                        .ToArray();
+
+                foreach (string file in files)
+                {
+                    string fileContent = File.ReadAllText(file);
+                    if (fileContent.IndexOf(searchString, comparisonType) >= 0)
+                    {
+                        result = "Found matching content in file: " + file;
+                        searchResults.Add(result);
+                    }
+                }
+            }
+            else
+            {
+                result = "Folder does not exist: " + currentFolder;
+                searchResults.Add(result);
+            }
+        }
+
+        private void searchLocationBox1_TextChanged(object sender, EventArgs e)
+        {
+            folderPath = searchLocationBox1.Text;
+        }
+
+        private void searchStringTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            searchString = searchStringTextBox1.Text;
+        }
+
+        private void searchButton1_Click(object sender, EventArgs e)
+        {
+            SearchInFolder(folderPath); 
+            consoleOutRichTextBox1.Text = string.Join(Environment.NewLine, searchResults);
+        }
+    }
+}
