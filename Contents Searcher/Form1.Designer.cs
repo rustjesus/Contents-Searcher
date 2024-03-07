9903b35541cc7035491ea95ff6163836c4c9610f
rustjesus <yeric12345@gmail.com>
2024-03-06 19:40:42 -0600

    named form

--- Contents Searcher/Form1.Designer.cs
+++ Contents Searcher/Form1.Designer.cs
@@ -100,7 +100,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchLocationBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CPP Contents Searcher";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
